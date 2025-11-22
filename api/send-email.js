8bb649e7c78310d12ccdac3a1fdaa47bb422b88f
devaadz <devawaow@gmail.com>
2025-11-23 06:13:00 +0700

    send_emil pesan

--- api/send-email.js
+++ api/send-email.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
         await transporter.sendMail({
             from: `"${name}" <${email}>`,
             to: process.env.MY_GMAIL,
-            subject: "Pesan Baru dari Form Contact",
+            subject: "Pesan Baru dari Form Contact di protofolio devaadzany.vercel.app/",
             html: `
                 <h3>Nama: ${name}</h3>
                 <h4>Email: ${email}</h4>
