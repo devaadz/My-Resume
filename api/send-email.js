139f0e781bd844c5d8844f6697ac2116715b1c8c
devaadz <devawaow@gmail.com>
2025-11-23 06:05:06 +0700

    add json

--- api/send-email.js
+++ api/send-email.js
@@ -1,6 +1,4 @@
-const nodemailer = require("nodemailer");
-
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Method not allowed" });
     }
@@ -10,6 +8,8 @@ module.exports = async (req, res) => {
     if (!name || !email || !message) {
         return res.status(400).json({ error: "Missing fields" });
     }
+
+    const nodemailer = require("nodemailer");
 
     const transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -21,7 +21,7 @@ module.exports = async (req, res) => {
 
     try {
         await transporter.sendMail({
-            from: email,
+            from: process.env.MY_GMAIL,
             to: process.env.MY_GMAIL,
             subject: `Pesan Baru dari ${name}`,
             html: `
@@ -31,10 +31,10 @@ module.exports = async (req, res) => {
             `
         });
 
-        return res.status(200).json({ success: true, message: "Email terkirim!" });
+        res.status(200).json({ success: true, message: "Email terkirim!" });
 
     } catch (error) {
         console.error(error);
-        return res.status(500).json({ error: "Gagal mengirim email" });
+        res.status(500).json({ error: "Gagal mengirim email" });
     }
-};
+}
